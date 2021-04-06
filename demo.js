04fa33d4940bb1260c30259d5f0bf3d43f1af72a
Sukaina Charaf Eddine <sukaina.kabalan01@gmail.com>
2021-04-06 09:22:29 +0000

    Automatically backed up by Learn

--- demo.js
+++ demo.js
@@ -10,9 +10,9 @@ let colorStates = {
   "": "red"
 };
 
-// STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
-// Without JavaScript, clicking on these heart shapes does nothing. Uncomment
-// this code and refresh the demo page.
+ STEP 1: This code is what lets JavaScript find the elements that we want to make clickable.
+ Without JavaScript, clicking on these heart shapes does nothing. Uncomment
+this code and refresh the demo page.
 
 // let articleHearts = document.querySelectorAll(".like-glyph");
 
